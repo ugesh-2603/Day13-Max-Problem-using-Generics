70a058dc2d191e3f0871bcff7daf6fe6008ebe77
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-16 07:59:42 +0530

    [ugesh - UC3-Maximum-String ]

--- Day13-Max-Problem-using-Generics/Program.cs
+++ Day13-Max-Problem-using-Generics/Program.cs
@@ -5,15 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to finding the maximum using Generics");
-            Console.WriteLine("Maximum of 5.5, 3.1, 7.9:\t" + FindMax(5.7f, 3.1f, 7.9f)); //7.9
-            Console.WriteLine("Maximum of 1.5, 9.5, 2.7:\t" + FindMax(1.2f, 9.5f, 2.6f)); //9.5
-            Console.WriteLine("Maximum of 6.0, 4.6, 5.9:\t" + FindMax(6.0f, 4.3f, 5.8f)); //6.0
-            Console.WriteLine("Maximum of 99.9, 0.0, -5.5:\t" + FindMax(99.9f, 0.0f, -5.5f)); //99
+            Console.WriteLine("Maximum of Apple, Peach, Blueberry:\t" + FindMax("Apple", "Peach", "Banana")); //Peach
+            Console.WriteLine("Maximum of Mango, Orange, Banana:\t" + FindMax("Mango", "Orange", "Berry")); //Orange
+            Console.WriteLine("Maximum of Pineapple, Guava, Cherry:\t" + FindMax("Pineapple", "Guava", "Cherry")); //Pineapple
+            Console.WriteLine("Maximum of Nine, Zero, Five:\t" + FindMax("Nine", "Zero", "Five")); //
         }
 
-        public static float FindMax(float a, float b, float c)
+        public static string FindMax(string a, string b, string c)
         {
-            float max = a;
+            string max = a;
 
             if (b.CompareTo(max) > 0)
             {
@@ -31,5 +31,4 @@
     }
 }
 
-        
-    
+
