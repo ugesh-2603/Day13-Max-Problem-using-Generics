b6d8f662182b33b6a614276f667900276d9a9ffb
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-16 07:39:07 +0530

    [ugesh - Welcome note ]

---
+++ Day13-Max-Problem-using-Generics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day13_Max_Problem_using_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to finding the maximum using Generics");
+        }
+    }
+}
