7c5881bb24c7cc700e3d0b0d30df0bc667c32ee1
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-17 07:17:34 +0530

    [ugesh - Refractor-2 ]

--- Day13-Max-Problem-using-Generics/Program.cs
+++ Day13-Max-Problem-using-Generics/Program.cs
@@ -5,15 +5,39 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to finding the maximum using Generics");
-            Console.WriteLine("Maximum of 5, 6, 3:\t" + FindMax(5, 6, 3)); // 6
-            Console.WriteLine("Maximum of 1.5, 2.3, -0.9:\t" + FindMax(1.5f, 2.3f, -0.9f)); // 2.3
-            Console.WriteLine("Maximum of Apple, Peach, Banana:\t" + FindMax("Apple", "Peach", "Banana")); // Peach
+            var intMaxTester = new MaximumTester<int>(5, 6, 3);
+            Console.WriteLine("Maximum of 5, 6, 3: " + intMaxTester.TestMaximum()); // 6
+
+            var floatMaxTester = new MaximumTester<float>(1.5f, 2.3f, 0.9f);
+            Console.WriteLine("Maximum of 1.5, 2.3, 0.9: " + floatMaxTester.TestMaximum()); // 2.3
+
+            var stringMaxTester = new MaximumTester<string>("Apple", "Peach", "Banana");
+            Console.WriteLine("Maximum of Apple, Peach, Banana: " + stringMaxTester.TestMaximum()); // Peach
         }
 
-        public static T FindMax<T>(T a, T b, T c) where T : IComparable<T>
+    }
+
+    public class MaximumTester<T> where T : IComparable<T>
+    {
+        private T a;
+        private T b;
+        private T c;
+
+        public MaximumTester(T a, T b, T c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public T TestMaximum()
+        {
+            return FindMax(a, b, c);
+        }
+
+        public static T FindMax(T a, T b, T c)
         {
             T max = a;
-
 
             if (b.CompareTo(max) > 0)
             {
@@ -25,10 +49,8 @@
                 max = c;
             }
 
-
             return max;
         }
-
     }
 }
 
