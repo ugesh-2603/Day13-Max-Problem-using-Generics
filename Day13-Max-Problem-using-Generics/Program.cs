10da008353a3e42547e4c3715120b250679a7147
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-17 07:30:07 +0530

    [ ugesh - UC5-Print-Test-Maximimum ]

--- Day13-Max-Problem-using-Generics/Program.cs
+++ Day13-Max-Problem-using-Generics/Program.cs
@@ -6,13 +6,16 @@
         {
             Console.WriteLine(" Welcome to finding the maximum using Generics");
             var intMaxTester = new MaximumTester<int>(5, 6, 3, 8, 2);
-            Console.WriteLine("Maximum of 5, 6, 3, 8, 2: " + intMaxTester.TestMaximum()); // 8
+            //Console.WriteLine("Maximum of 5, 6, 3, 8, 2: " + intMaxTester.TestMaximum()); // 8
+            intMaxTester.TestMaximum(); // prints "The maximum value is: 8"
 
             var floatMaxTester = new MaximumTester<float>(1.5f, 2.3f, 0.9f, 3.4f, 0.2f);
-            Console.WriteLine("Maximum of 1.5, 2.3, 0.9, 3.4, 0.2: " + floatMaxTester.TestMaximum()); // 3.4
+            //Console.WriteLine("Maximum of 1.5, 2.3, 0.9, 3.4, 0.2: " + floatMaxTester.TestMaximum()); // 3.4
+            floatMaxTester.TestMaximum(); // prints "The maximum value is: 3.4"
 
             var stringMaxTester = new MaximumTester<string>("Apple", "Peach", "Banana", "Mango", "Orange");
-            Console.WriteLine("Maximum of Apple, Peach, Banana, Mango, Orange: " + stringMaxTester.TestMaximum()); // Peach
+            //Console.WriteLine("Maximum of Apple, Peach, Banana, Mango, Orange: " + stringMaxTester.TestMaximum()); // Peach
+            stringMaxTester.TestMaximum(); // prints "The maximum value is: Peach"
         }
     }
 
@@ -27,7 +30,9 @@
 
         public T TestMaximum()
         {
-            return FindMax(elements.ToArray());
+            T max = FindMax(elements.ToArray());
+            PrintMax(max);
+            return max;
         }
 
         public static T FindMax(params T[] elements)
@@ -36,6 +41,11 @@
             sortedElements.Sort();
 
             return sortedElements.Last();
+        }
+
+        private void PrintMax(T max)
+        {
+            Console.WriteLine("The maximum value is: " + max);
         }
     }
 }
